8f17018fa840954f2ee2b511d3c8bdcf2e224c2a
✨ Jakeline Melo ✨ <jakelinemello@live.com>
2026-06-01 17:26:32 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [react()],
+  tanstackStart: {
+    server: {
+      entry: "server",
+    },
+
+    nitro: {
+      preset: "vercel",
+    },
+  },
 });
